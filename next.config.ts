9016bfec1f73ecfc8ef6e9536565e1a202321e8f
Chakeereen <65143052@g.cmru.ac.th>
2025-03-06 21:11:28 +0700

    adas

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig = {
   images: {
     remotePatterns: [
+      // {
+      //   protocol: "http",
+      //   hostname: "localhost",
+      //   port: "1337",
+      //   pathname: "/uploads/**/*",
+      // },
+
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
+        protocol: "https",
+        hostname: "https://server-g8da.onrender.com",
+        port: "",
         pathname: "/uploads/**/*",
       },
     ],
