6d75784a1c224149fee2f8624eaf01d913d9b301
Chakeereen <65143052@g.cmru.ac.th>
2025-03-06 16:58:30 +0700

    asd

--- src/app/components/blocks/rich-text-block.tsx
+++ src/app/components/blocks/rich-text-block.tsx
@@ -4,16 +4,16 @@ import {
   type BlocksContent,
 } from "@strapi/blocks-react-renderer";
 import Image from "next/image";
-
+ 
 export interface RichTextBlock {
   __component: "blocks.rich-text";
   id: number;
   content: BlocksContent;
 }
-
+ 
 // This renderer is using Strapi's Rich Text renderer.
 // https://github.com/strapi/blocks-react-renderer
-
+ 
 export function RichTextBlock({ block }: { block: RichTextBlock }) {
   return (
     <div className="richtext">
